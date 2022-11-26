f2e62244eb4fa20fab4ba2526173691534c5e033
Muhammad Yudhistira <myudhistira07@gmail.com>
2022-11-27 06:15:21 +0700

    test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,26 +5,26 @@ use Hash;
 use Session;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-class CustomAuthController extends Controller
+class AuthController extends Controller
 {
     public function index()
     {
         return view('auth.login');
-    }  
-      
+    }
+
     public function customLogin(Request $request)
     {
         $request->validate([
             'email' => 'required',
             'password' => 'required',
         ]);
-   
+
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
             return redirect()->intended('dashboard')
                         ->withSuccess('Signed in');
         }
-  
+
         return redirect("login")->withSuccess('Login details are not valid');
     }
 
@@ -32,18 +32,18 @@ class CustomAuthController extends Controller
     {
         return view('auth.registration');
     }
-      
+
     public function customRegistration(Request $request)
-    {  
+    {
         $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6',
         ]);
-           
+
         $data = $request->all();
         $check = $this->create($data);
-         
+
         return redirect("dashboard")->withSuccess('You have signed-in');
     }
 
@@ -54,21 +54,21 @@ class CustomAuthController extends Controller
         'email' => $data['email'],
         'password' => Hash::make($data['password'])
       ]);
-    }    
-    
+    }
+
     public function dashboard()
     {
         if(Auth::check()){
             return view('dashboard');
         }
-  
+
         return redirect("login")->withSuccess('You are not allowed to access');
     }
-    
+
     public function signOut() {
         Session::flush();
         Auth::logout();
-  
+
         return Redirect('login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\BarangController;
 use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\PembelianController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,26 +17,28 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/login', [Authcontroller::class, 'login'])->name("Login");
+Route::get('/login', [Authcontroller::class, 'Customlogin'])->name("Login");
 Route::post('/login', [AuthController::class, 'authenticate']);
 Route::post('/logout', [AuthController::class, 'Logout']);
 
-Route::middleware("auth")->group(function(){
+
     Route::get('/barang/list', [BarangController::class, 'index']);
     Route::post('/barang', [BarangController::class, 'store']);
     Route::get('/barang/create', [BarangController::class, 'create']);
     Route::get('/barang/{barang}', [BarangController::class, 'destroy']);
     Route::put('/barang/{barang}', [BarangController::class, 'update']);
     Route::get('/barang/{barang}/edit', [BarangController::class, 'edit']);
-    
+
     Route::get('/user/list',[UserController::class, 'index']);
-    
+
     Route::get('/penjualan/list', [PenjualanController::class, 'index']);
     Route::get('/penjualan/create', [PenjualanController::class, 'create']);
     Route::post('/penjualan', [PenjualanController::class, 'store']);
-    
+
     Route::get('/pembelian/list', [PembelianController::class, 'index']);
     Route::get('/pembelian/create', [PembelianController::class, 'create']);
     Route::post('/pembelian', [PembelianController::class, 'store']);
-});
